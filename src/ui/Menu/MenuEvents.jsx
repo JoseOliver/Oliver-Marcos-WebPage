9ab1262f750823db76e7df79a2c8248a0b97bdf8
Jose Andrés <oliver_ja@outlook.com>
2025-01-15 23:00:30 +0100

    update: now menu can be opened with touch events too

--- src/ui/Menu/MenuEvents.jsx
+++ src/ui/Menu/MenuEvents.jsx
@@ -8,27 +8,44 @@ export function addEventsToMenu(
   menuApi,
   setMenuVisible
 ) {
-  // event listeners for movement
-  window.addEventListener("mousemove", (evt) => {
-    // on move
+  // func to add a handler to multiple listeners
+  const addMultipleListeners = (element, events, handler, useCapture) => {
+    const addEvent = (one) => {
+      element.addEventListener(one, (evt) => handler(evt), useCapture);
+    };
+    events.forEach(addEvent);
+  };
+
+  // event handlers for movement
+  const onMove = (evt) => {
     if (tapDown !== false) {
-      let distance = evt.clientX - tapDown;
+      let X;
+      if (evt.type == "mousemove") X = evt.clientX;
+      else if (evt.type == "touchmove") X = evt.touches[0].clientX;
+      let distance = X - tapDown;
       if (tracking) handleMenuDrag(distance, menuApi);
     }
-  });
-  window.addEventListener("mousedown", (evt) => {
-    // on mousedown
-    tapDown = evt.clientX;
-    if (evt.pageX < 50) {
+  };
+
+  const onDown = (evt) => {
+    let X;
+    if (evt.type == "mousedown") X = evt.clientX;
+    else if (evt.type == "touchstart") X = evt.touches[0].clientX;
+    tapDown = X;
+    if (X < 50) {
+      //antes PageX
       tracking = true;
     }
     closeMenuOnClick(evt.target, setMenuVisible, menuApi);
-  });
-  window.addEventListener("mouseup", (evt) => {
-    //on mouseup
-    tapUp = evt.clientX;
+  };
+
+  const onUp = (evt) => {
+    let X;
+    if (evt.type == "mouseup") X = evt.clientX;
+    else if (evt.type == "touchend") X = evt.changedTouches[0].clientX;
+    tapUp = X;
     let distance = tapUp - tapDown;
-    if (tracking && distance > 150) {
+    if (tracking && distance >= 150) {
       showMenu(menuApi);
       setMenuVisible(true);
     }
@@ -43,5 +60,12 @@ export function addEventsToMenu(
     tapDown = false;
     tapUp = false;
     tracking = false;
-  });
+  };
+
+  // add move listeners
+  addMultipleListeners(window, ["touchmove", "mousemove"], onMove, true);
+  // add down listeners
+  addMultipleListeners(window, ["touchstart", "mousedown"], onDown, true);
+  // add up listeners
+  addMultipleListeners(window, ["touchend", "mouseup"], onUp, true);
 }
