f26b904336f6a65996bb5ab3bcaecb0d5464f778
Jose Andrés <oliver_ja@outlook.com>
2025-01-24 19:07:34 +0100

    fix: minor fixes and warning flags updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,28 +5,40 @@ import App from "./App";
 import Index from "./ui/Index/Index";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <>Pagina no encontrada</>,
+      children: [
+        {
+          path: "/about",
+          element: <>Acerca de</>,
+        },
+        {
+          path: "/products",
+          element: <>Productos</>,
+        },
+        {
+          path: "/",
+          element: <Index />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <>Pagina no encontrada</>,
-    children: [
-      {
-        path: "/about",
-        element: <>Acerca de</>,
-      },
-      {
-        path: "/products",
-        element: <>Productos</>,
-      },
-      {
-        path: "/",
-        element: <Index />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} future={{ v7_startTransition: true }} />
 );
--- src/ui/Index/Index.jsx
+++ src/ui/Index/Index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 // import "./index.css";
 
 const Index = () => {
-  console.log(self);
   return <div height={window.visualViewport.height}>Index</div>;
 };
 
